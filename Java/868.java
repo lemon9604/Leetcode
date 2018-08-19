9fc6652f4f6e5659a91256a3bf5c9b4a05e1f8e5
lemon9604 <lemon9604@163.com>
2018-08-19 22:26:12 +0800

    868.java

--- Java/868.java
+++ Java/868.java
@@ -1,8 +1,27 @@
 public int binaryGap(int N){
 	int beishu = 1;
+	int yushu = 0;
 	int max = 0;
+	int cnt = 0;
 	while (N != 0){
-		
+		yushu = N % 2;
+		N = N / 2;
+		if (yushu == 1){
+			if (cnt == 0){
+				cnt = 1;
+			}
+			else{
+				if (max < cnt){
+					max = cnt;
+				}
+				cnt = 1;
+			}
+		}
+		else{
+			if (cnt != 0){
+				cnt ++;
+			}
+		}
 	}
 	return max;
 }
