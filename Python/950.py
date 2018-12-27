0ee862c7be1fd243ecb03ce340f787607fa57cc5
Yiqing <lemon9604@163.com>
2018-12-26 19:30:21 -0500

    701. Insert into a Binary Search Tree

--- Python/950.py
+++ Python/950.py
@@ -11,3 +11,23 @@ class Solution:
         for i in range(len(deck) - 2, -1, -1):
             rt = [deck[i]] + [rt[len(rt) - 1]] + rt[0:-1]
         return rt
+
+
+# class Solution:
+#     def deckRevealedIncreasing(self, deck):
+#         """
+#         :type deck: List[int]
+#         :rtype: List[int]
+#         """
+#         from collections import deque
+#
+#         i = deque()
+#         for c in range(len(deck)):
+#             i.append(c)
+#
+#         ans = [None] * len(deck)
+#         for card in sorted(deck):
+#             ans[i.popleft()] = card
+#             if i:
+#                 i.append(i.popleft())
+#         return ans
